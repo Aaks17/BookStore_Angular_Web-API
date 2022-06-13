e6eebfaa26f8453187cf20f37fc6b673a56e8a42
aakass <aakasselango@gmail.com>
2022-06-13 14:45:33 +0530

    AddStoreApiandStoreApp

---
+++ StoreAPI/StoreAPI/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreAPI.Models;
+
+namespace StoreAPI.Data
+{
+    public class StoreContext:DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options):base(options)
+        {
+        }
+
+        public DbSet<Store> Stores { get; set; }
+    }
+}
